793d5defcd9ee9542eb2a9b2eb5159ca2a335656
fatemehoseinzadeh <ftmdevhoseinzadeh@gmail.com>
2024-06-29 11:53:25 +0330

    detail page updated

--- src/app/detail/[slug]/page.tsx
+++ src/app/detail/[slug]/page.tsx
@@ -2,14 +2,8 @@ import { getProductBySlug } from "@/lib/data";
 import Image from "next/image";
 import React from "react";
 
-const Detail =  async ({
-  params,
-}: {
-  params: { slug:string };
-}) => {
-  
-  const product =await getProductBySlug(params.slug)
-
+const Detail = async ({ params }: { params: { slug: string } }) => {
+  const product = await getProductBySlug(params.slug);
 
   return (
     <>
@@ -24,7 +18,7 @@ const Detail =  async ({
             className="w-full h-96 object-contain"
           />
         </div> */}
-        <div className="w-5/12 me-4" style={{backgroundColor:"#F2F2F4"}}>
+        <div className="w-5/12 me-4" style={{ backgroundColor: "#F2F2F4" }}>
           <Image
             alt={product?.name || ""}
             src={product?.image || "/images/Rectangle 119.png"}
@@ -34,26 +28,55 @@ const Detail =  async ({
             className="w-full h-96 object-contain"
           />
         </div>
-        <div className="w-6/12">
-          <h1 className="font-bold text-3xl text-pink-900 pt-4">
-            {product?.name}
-          </h1>
-          <p className="text-md font-bold mt-2">Anti-aging face serum</p>
-          <p className="text-sm">
-            {product?.description}
-          </p>
-          <p className="text-md mt-2 font-bold">${product?.price}</p>
-          <Image
-            alt=""
-            src="/images/product_detail.png"
-            width={"300"}
-            height={"200"}
-            sizes="60vw"
-            className="mt-20"
-          />
+        <div className="w-6/12 flex flex-col justify-between items-end">
+          <div>
+            <h1 className="font-bold text-3xl text-pink-900 pt-4">
+              {product?.name}
+            </h1>
+            <p className="text-md font-bold mt-2">Anti-aging face serum</p>
+            <p className="text-sm">{product?.description}</p>
+            <p className="text-md mt-2 font-bold">${product?.price}</p>
+          </div>
+          <div className="w-2/3 bg-pink-50 rounded p-4">
+            <div className="flex my-2">
+              <Image
+                alt=""
+                src="/images/Vector.png"
+                width={"18"}
+                height={"14"}
+                className="me-2"
+              />
+              <p className="text-sm ">
+                receive 2 free samples when you spend $100
+              </p>
+            </div>
+            <div className="flex my-2">
+              <Image
+                alt=""
+                src="/images/Vector-2.png"
+                width={"18"}
+                height={"14"}
+                className="me-2"
+              />
+              <p className="text-sm ">
+                receive free 1-2-1 expert advice in branches
+              </p>
+            </div>
+            <div className="flex my-2">
+              <Image
+                alt=""
+                src="/images/Vector-1.png"
+                width={"18"}
+                height={"10"}
+                className="me-2"
+              />
+              <p className="text-sm ">
+                receive $2 when you return 5 empty containers
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-      
 
       <div className="m-4">
         <h1 className="font-bold text-xl pt-4">How To Apply</h1>
